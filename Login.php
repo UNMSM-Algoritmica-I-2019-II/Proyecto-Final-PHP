c5bfea48ee633929b446e059b5928f92f9d705ee
Josue Estrada <josue.estrada@unmsm.edu.pe>
2019-11-22 01:37:57 -0500

    Agregado Login, Conexion a la BBDD

---
+++ Login.php
@@ -0,0 +1,66 @@
+<?php
+include 'conexion.php';
+session_start();
+
+if (isset($_POST['username']) and isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $_SESSION['username'] = $username; // Variable glogal 
+    // Conexion a la DB
+    $link = mysqli_connect($host, $user, $pass) or die("Falló la conexión");
+    mysqli_select_db($link, $dbname) or die("Falló la conexión a la base de datos");
+
+    // Query para administrador
+    $query = "SELECT U.ID_Username FROM usuario AS U, administrador AS A WHERE U.ID_Username = '$username' AND U.Password = '$password' AND U.ID_Username = A.ID_Username";
+    $result = mysqli_query($link, $query) or die(mysqli_error($link));
+    if ($result == false) {
+        echo "La consulta falló";
+        exit();
+    }
+    if (mysqli_num_rows($result) == 1) { // Hay un match
+        header('Location:PanelAdministrador.php');
+    }
+
+    // Query para usuarios
+    $query = "SELECT ID.Username FROM usuario WHERE ID_Username = '$username' AND Password = '$password'";
+    $result = mysqli_query($link, $query) or die(mysqli_errno($link));
+    if ($result == false) {
+        echo "La consulta falló";
+        exit();
+    }
+    if (mysqli_num_rows($result) == 1) { // Hay un match
+        header('Location:PanelUsuario.php');
+    } else {
+        $error = "Usuario o Constraseña incorrecto";
+    }
+    echo "$error";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="" method="post">
+        <table>
+            <tr>
+                <td>Usuario</td>
+                <td><input type="text" name="username" required/></td>
+            </tr>
+            <tr>
+                <td>Contraseña</td>
+                <td><input type="text" name="password" required/></td>
+            </tr>
+        </table>
+        <input type="Submit" value="Iniciar Sesión"/>
+    </form>
+    <form action="NuevoRegistro.php" method="post">
+        <input type="Submit" value="Crear Cuenta"/>
+    </form>
+
+</body>
+</html>
---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+
+$user = "root";
+$pass = "";
+$host = "localhost";
+$dbname = "BIBLIOTECADB";
+
+?>
+
