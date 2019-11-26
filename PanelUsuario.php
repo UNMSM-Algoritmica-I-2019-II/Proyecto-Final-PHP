fb2447655f9115926142c71fe83b892a37fb809c
Josue Estrada <josue.estrada@unmsm.edu.pe>
2019-11-25 22:30:48 -0500

    Agregado html

---
+++ PanelUsuario.php
@@ -0,0 +1,31 @@
+<?php
+include 'conexion.php'; 
+
+session_start();  
+$username = $_SESSION['username'];
+	
+?> 
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Panel de Usuario</h2>
+        <form action="BuscaryPedir.php" method="post">
+            <input type="submit" value="Buscar Libro"/>
+        </form>
+
+        <form action="RetornarLibro.php" method="post">
+            <input type="submit" value="Retornar Libro"/>
+        </form>
+
+        <form action="Login.php" method="post">
+            <input type="submit" value="Cerrar Sesión"/>
+        </form>
+    </body>
+</html>
+
+
