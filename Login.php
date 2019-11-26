3afaf601af492c2943daf0ac905121aa304d5ee3
Josue Estrada <josue.estrada@unmsm.edu.pe>
2019-11-25 23:36:44 -0500

    Arreglado login

--- Login.php
+++ Login.php
@@ -2,39 +2,40 @@
 include 'conexion.php';
 session_start();
 
-if (isset($_POST['username']) and isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    $_SESSION['username'] = $username; // Variable glogal 
-    // Conexion a la DB
-    $link = mysqli_connect($host, $user, $pass) or die("Falló la conexión");
-    mysqli_select_db($link, $dbname) or die("Falló la conexión a la base de datos");
-
-    // Query para usuarios
-    $query = "SELECT ID.Username FROM usuario WHERE ID_Username = '$username' AND Password = '$password'";
-    $result = mysqli_query($link, $query) or die(mysqli_errno($link));
+if (isset($_POST['username']) and isset($_POST['password'])) { //check null
+    $username = $_POST['username']; // text field for username 
+    $password = $_POST['password']; // text field for password
+// Guarda datos de sesion
+    $_SESSION['username'] = $username;
+// Conecta a la BBDD
+    $link = mysqli_connect($host, $user, $pass) or die("No puede conectarse");
+    mysqli_select_db($link, $dbname) or die("No se puede seleccionar la base de datos");
+    
+// Consulta sql
+    $query = "Select ID_Username From usuario Where ID_Username = '$username' AND Password = '$password'";
+// Verifica la consulta sql
+    $result = mysqli_query($link, $query) or die(mysqli_error($link));
     if ($result == false) {
-        echo "La consulta falló";
+        echo 'La consulta falló';
         exit();
     }
-    // Verificacion
-    if (mysqli_num_rows($result) == 1) { // Hay un match
-        header('Location: PanelUsuario.php');  
+    // Si encontro un match, redirige al PanelUsuario.php 
+    if (mysqli_num_rows($result) == 1) {
+        header('Location: PanelUsuario.php');
     } else {
-        $error = "Usuario o Constraseña incorrecto";
+        $err = 'ERROR: Usuario o Contraseña incorrecto';
     }
-    echo "$error";
+    // Muestra el mensaje de error
+    echo "$err";
 }
-?>
+?>	
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
-        <form action="" method="post">
+    <form action="" method="post">
         <table>
             <tr>
                 <td>Usuario</td>
