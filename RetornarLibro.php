84b4ebb062fb1ccfeb7e349d84b2ffada9f83121
Josue Estrada <josue.estrada@unmsm.edu.pe>
2019-11-25 22:31:03 -0500

    Agregado

---
+++ RetornarLibro.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h2>Retornar Libro</h2>
+        
+        
+        <form action="PanelUsuario.php" method="post">
+            <input type="submit" value="Retornar"/>
+        </form>
+        
+    </body>
+</html>
+
+
