936d1c9543b82656022414a1753b2caf0c0aa1e6
Josue Estrada <josue.estrada@unmsm.edu.pe>
2019-11-29 00:25:04 -0500

    Formatting html

--- NuevoRegistro.php
+++ NuevoRegistro.php
@@ -28,34 +28,33 @@ if (isset($_POST['username']) and isset($_POST['password']) and isset($_POST['cp
     }
 }
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="style.css">
+        <title>Registro</title>
     </head>
     <body>
         <form action="" method="post">
-        <table>
-            <tr>
-                <td>Usuario</td>
-                <td><input type="text" name="username" required/></td>
-            </tr>
-            <tr>
-                <td>Contraseña</td>
-                <td><input type="text" name="password" required/></td>
-            </tr>
-            <tr>
-                <td>Confirmar Contraseña</td>
-                <td><input type="text" name="cpassword" required/></td>
-            </tr>
-        </table>
-        <input type="Submit" value="Registrar"/>
-    </form>
-    <form action="Login.php" method="post">
-        <input type="Submit" value="Regresar"/>
-    </form>
-
-</body>
+            <table>
+                <tr>
+                    <td>Usuario</td>
+                    <td><input type="text" name="username" required/></td>
+                </tr>
+                <tr>
+                    <td>Contraseña</td>
+                    <td><input type="text" name="password" required/></td>
+                </tr>
+                <tr>
+                    <td>Confirmar Contraseña</td>
+                    <td><input type="text" name="cpassword" required/></td>
+                </tr>
+            </table>
+            <input type="Submit" value="Registrar"/>
+        </form>
+        <form action="Login.php" method="post">
+            <input type="Submit" value="Regresar"/>
+        </form>
+    </body>
 </html>
